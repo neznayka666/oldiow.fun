2bc6c2cb53eb53cc6d47f00379853ae83249dede
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-04-26 21:46:26 +0300

    Update wears.php

--- inc/inc/wears.php
+++ inc/inc/wears.php
@@ -265,7 +265,7 @@ if (@$http->get["rune_join"])
 	{
 		remove_weapon ($weared_id,$weared_wp);
 		$sk = explode("_",$rune["id_in_w"]);
-		$add_rune = "<b>Руна:</b> ".$rune["name"]." (".$sk[2].": <b>+".$sk[1]."</b>)";
+		$add_rune = "<b>Руна:</b> ".$rune["name"]." (".$sk[2].": <b>+".$sk[1]."</b>)\r\n";
 		$db->sql("UPDATE wp SET `".$sk[2]."`=`".$sk[2]."`+".$sk[1].",slots=slots-1,price=price+".sqrt($rune["price"]).",
 		`name`='".$weared_name." [Р]', `describe`=`".$v["describe"]."`+".$add_rune." WHERE id=".$weared_id."");
 		if ($sk[2]=="udmax")$db->sql("UPDATE wp SET `udmin`=`udmin`+1 WHERE id=".$weared_id."");
