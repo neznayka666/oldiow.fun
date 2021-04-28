644a559410585044019fb127c02ea9a9faddae4f
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-04-28 08:10:51 +0300

    Update fightn.js

--- js/fightn.js
+++ js/fightn.js
@@ -88,7 +88,7 @@ var s='';
 if (travm<10) travm=10;
 if (oruj==1)s='<img src="images/icons/battle/1.png" title="Тип боя: С оружием">'; else
 s='<img src="images/icons/battle/1.png" title="Тип боя: Рукопашный">';
-s = '<input class="submit" type="button" value="Обновить" onclick="location=\'main.php?vs_id=' + persvs_id + '\';" style="width:110px;margin:0;"> <hr><table width="400" cellspadding="0" cellspacing="0" style="height:30px;margin:0 auto;"><tr><td align="center">'+s;
+s = '<!--input class="submit" type="button" value="Обновить" onclick="location=\'main.php?vs_id=' + persvs_id + '\';" style="width:110px;margin:0;"> <hr!--><table width="400" cellspadding="0" cellspacing="0" style="height:30px;margin:0 auto;"><tr><td align="center">'+s;
 s+=' | <img src="images/icons/battle/light.png" title="Травматичность: '+travm+' %">';
 if(timeout)
 	setTimeout("location='main.php'",timeout*1000);
