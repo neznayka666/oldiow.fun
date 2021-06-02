579dbc34c5afab538c2537d09cc7ea194304ad2c
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-06-02 22:15:12 +0300

    Update alchemy.php

--- inc/inc/alchemy.php
+++ inc/inc/alchemy.php
@@ -196,7 +196,7 @@
 	if ($count_r>0 and $cell_type==6) echo "<a href='main.php?alchemy_go=1&inv=cat5' class=but>Варить</a>";
 	echo "</center>";
 	
-	$res = $db->sql("SELECT * FROM `wp` WHERE `uidp`=".$player->pers["uid"]." and weared=0 and p_type=200");
+	$res = $db->sql("SELECT * FROM `wp` WHERE `uidp`=".$player->pers["uid"]." and weared=0");
 echo "<center class=but><table width=90%>";
 $unique_herbal = '';
 $counter=0;
