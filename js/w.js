391d1a2cdaf5356faaef9022b521e040153a2a04
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-07-20 21:21:52 +0300

    Update w.js

--- js/w.js
+++ js/w.js
@@ -1,5 +1,5 @@
 
-function show_w (name,sht,img,d,m_d,cena,pric,stype,dprice,stype,art,attr,describe,present,clan_sign,clan_name,slots,radius,arrows,arrows_max,arrow_name,z_time,weight,index,trbs)
+function show_w (name,sht,img,d,m_d,cena,pric,dprice,stype,art,attr,describe,present,clan_sign,clan_name,slots,radius,arrows,arrows_max,arrow_name,z_time,weight,index,trbs)
 {
 	var text = '';
 	if (sht==1 || sht=='') sht=''; else sht = sht+'шт.';
