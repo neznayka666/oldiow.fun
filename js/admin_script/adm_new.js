c941c7b18b636a28a9c82260609d3d28b51d61be
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-07-30 13:57:42 +0300

    Update adm_new.js

--- js/admin_script/adm_new.js
+++ js/admin_script/adm_new.js
@@ -104,8 +104,8 @@ function editw()
 	var _params = params.split('@');
 	var np;
 	d.write('<div class=return_win id=main></div>');
-	d.write('<table border="1" width="100%" cellspacing="5" cellpadding="5" bordercolorlight=#C0C0C0 bordercolordark=#FFFFFF>');
-	d.write('<tr><td class=login align=center width=50% id=opts>СВОЙСТВА</td><td class=login width=50% align=center id=reqs>ТРЕБОВАНИЯ</td></tr>');
+	d.write('<table border="1" width="100%" cellspacing="5" cellpadding="5" class="whiteBlock margin-5">');
+	d.write('<tr><td align=center width=50% id=opts>СВОЙСТВА</td><td width=50% align=center id=reqs>ТРЕБОВАНИЯ</td></tr>');
 	d.write('<tr><td class=inv id=o width=50% valign=bottom></td><td class=inv id=t width=50% valign=bottom></td></tr>');
 	d.write('</table><div class=return_win id=sec_params></div>');
 	d.write('</form>');
