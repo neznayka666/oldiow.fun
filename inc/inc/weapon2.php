44a6de24cd344f033ab0d3cf0f93349982b15900
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-11 17:54:22 +0300

    Update weapon2.php

--- inc/inc/weapon2.php
+++ inc/inc/weapon2.php
@@ -73,6 +73,7 @@ if ( !empty($v['id']) )
 		}
 		//if($v["material_show"]) $attrs.= '<tr><td>Материал: </td><td>'.$v["material_show"].'</td></tr>';
 		//else $attrs.= '<tr><td>Материал: </td><td><i>неизвестно</i></td></tr>';
+		$attrs.= 'stype: <i>'.$v["stype"].'</i><br>';
 		if ($rank_i>0) $attrs.= 'Мощь предмета: <i>'.$rank_i.'</i><br>';
 		$attrs .= '</td></tr></table>';
 		if (!$v_count) $attrs = '';
