91c7b5758b8a018cbe958a33b0507130a0981dfd
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-16 13:32:58 +0300

    Update wears.php

--- inc/inc/wears.php
+++ inc/inc/wears.php
@@ -320,7 +320,8 @@ case "s6":
 		`".$sk[2]."`=`".$sk[2]."`+".$sk[1].",
 		`slots`=slots-1,
 		`price`=price+".$rune["price"].",
-		`name`='".$weared_name." [Р]', 
+		`name`='".$weared_name." [Р]',
+		`upgratedRune`=upgratedRune+1,  
 		`describeRune`='".$weared_describe." ".$add_rune."'
 		WHERE id=".$weared_id."
 		");
