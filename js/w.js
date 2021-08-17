a54ea5abc73129d9c12572846915a1797f2559c4
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-17 16:43:27 +0300

    Update w.js

--- js/w.js
+++ js/w.js
@@ -66,6 +66,7 @@ text += (`<img src="/images/weapons/${img}.gif">`);
 
 if ((upgrated > 0) || (upgratedRune > 0)) { 
 	text += ('<div style="position: absolute; bottom: 2px; left: 2px;">');
+}
 	if (upgrated > 0) text += ('<div style="width:5px;height:5px;background:green;"></div>');
 	if (upgratedRune > 0) text += ('<div style="width:5px;height:5px;background:red;"></div>');
 	text += ('</div>');
