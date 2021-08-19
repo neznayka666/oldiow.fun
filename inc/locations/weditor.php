86e2db958a340fbb5c306cd92f5b646be1003edd
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-19 16:22:47 +0300

    Update weditor.php

--- inc/locations/weditor.php
+++ inc/locations/weditor.php
@@ -272,7 +272,7 @@ case "s6":
 }			
 			$db->sql("UPDATE wp SET 
 			`".$param."` = `".$param."` + ".($kk*$eq).",
-			`describeMF` = describeMF+'".$add_mf."',
+			`describeMF` = '".$describeMF." ".$add_mf."',
 			`upgrated`=upgrated+1,			
 			`price`=price+".UP_COST."*".$kk." 
 			WHERE 
@@ -281,6 +281,7 @@ case "s6":
 			$v[$param] += $kk*$eq;
 			$v["price"] += UP_COST * $kk;
 			$v["upgrated"]++;
+			$describeMF = $v["describeMF"];
 			$vesh = $v;
 			echo "<b class=green>Удача!</b>";
 			echo "<div class=but>";
