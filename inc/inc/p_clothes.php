7a5968963adf8fd2cdee19d17ea1379e9ecb3b64
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-19 12:12:59 +0300

    Update p_clothes.php

--- inc/inc/p_clothes.php
+++ inc/inc/p_clothes.php
@@ -80,6 +80,7 @@ while ($v=mysql_fetch_array($res))
 		if ($v["mf5"]) $dscr .= 'Пробой брони: <b>'.plus_param($v["mf5"])."%</b>@";
 		if ($v["hp"]) $dscr .= 'Уровень жизни: <b>'.plus_param($v["hp"])." HP</b>@";
 		if ($v["ma"]) $dscr .= 'Уровень энергии: <b>'.plus_param($v["ma"])." EP</b>@";
+		if ($v["d"]) $dscr .= 'Долговечность: <b>'.plus_param($v["d"]).' [<b>'.plus_param($v["d"])."</b>]</b>@";
 		//if ($v["slots"]) $dscr .= 'Слотов: <B>'.$v["slots"]."</B>@";
 		//if ($v["radius"]) $dscr .= 'Радиус поражения: <B>'.$v["radius"]."</B>@";
 	if ($v["type"]=="naruchi" and $na["image"]=$v["image"]) $na["id"]=$dscr;
