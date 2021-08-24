74730f6a6e528553fe1c805f12ae44694158c3cb
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-24 20:29:09 +0300

    Update instant.php

--- inc/locations/instant.php
+++ inc/locations/instant.php
@@ -8,13 +8,13 @@ echo '
 <h3 class="titleCity">Подземелье Драконов</h3>
 
 
-<table border="0" cellspacing="0" width="100%" class="greyBlock" style="margin:10px 0;">
+<!--table border="0" cellspacing="0" width="100%" class="greyBlock" style="margin:10px 0;">
 <tr>
 <td width="50%"><a href="main.php?do=1" class="'.(($do==1) ? 'blocked' : 'bga').'">В битву с драконом</a></td>
 <!--td width="50%"><a href="main.php?do=2" class="'.(($do==2) ? 'blocked' : 'bga').'">Лавка Драконоборца</a></!--td>
 <!--td width="30%"><a href="main.php?do=3" class="'.(($do==3) ? 'bga' : 'blocked').'">Стражник у врат подземелья</a></!--td-->	
 </tr>
-</table>
+</table-->
 
 ';
 
