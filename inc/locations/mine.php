f290538caf3bcc7a59202f16acf483c37142180b
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-25 21:56:12 +0300

    Update mine.php

--- inc/locations/mine.php
+++ inc/locations/mine.php
@@ -9,7 +9,7 @@ $tu = $db->sqla("SELECT * FROM mine WHERE x=".($player->pers["minex"])." and y="
 	$t = tme();
 	//$t = "";
 	$timep = 600;
-	if ($player->pers["priveleged"]>=1) {
+	if ($player->pers["priveleged"]>=100) {
 	$timed = 0; // 240
 	} else {
 		$timed = 240; // 240
