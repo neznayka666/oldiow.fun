ac2a8bb79023f47aad189dff5f8afdbeae8ee37a
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-26 17:07:16 +0300

    Update whouse.php

--- inc/locations/whouse.php
+++ inc/locations/whouse.php
@@ -299,7 +299,7 @@ echo '<div align="center">';
 						break;
 					}
 					
-            //say_to_chat ("s","Вы удачно выставили на аукцион <b>".$name_item."</b>. Стартовая цена лота: <b>".$price."</b> зм. Торги окончатся: <b>".date("d.m.Y H:i",$time)."</b>",1,$player->pers["user"],'*',0);
+            say_to_chat ("s","Вы удачно выставили на аукцион <b>".$res["upgrated"]."</b>. Стартовая цена лота: <b>".$price."</b> зм. Торги окончатся: <b>".date("d.m.Y H:i",$time)."</b>",1,$player->pers["user"],'*',0);
           }else{
             show_err('У вас недостаточно денег для выставления лота','e');
             say_to_chat ("s","У вас недостаточно денег для выставления лота",1,$player->pers["user"],'*',0);
