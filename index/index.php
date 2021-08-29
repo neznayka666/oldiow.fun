67d62f0a89f6cb2e53bd41cd4ee3ae855321f385
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-08-29 21:19:05 +0300

    Update index.php

--- index/index.php
+++ index/index.php
@@ -264,7 +264,7 @@ img {
 			<TR>
 				<TD ALIGN=LEFT VALIGN=TOP>
 <?php
-echo '<b class="title">Новости.</b><br /><br /><div class="news_middle">';
+echo '<div class="news_middle">';
 
 if ( isset($_POST['ntext']) and isset($_GET['subact']) and $pers != false )
 {
@@ -273,16 +273,10 @@ if ( isset($_POST['ntext']) and isset($_GET['subact']) and $pers != false )
 	$db->sql("UPDATE `lib_news` SET `coment`=coment+1 WHERE `id`='".$_GET['subact']."';");
 }
 
-if ( isset($_GET['del']) and isset($_GET['subact']) and $pers != false and $pers['priveleged']==1 )
-{
-	$db->sql("DELETE FROM `lib_news_coment` WHERE `id`='".$_GET['del']."' and `id_news`='".$_GET['subact']."' ;");
-	$db->sql("UPDATE `lib_news` SET `coment`=coment-1 WHERE `id`='".$_GET['subact']."';");
-}
-
 
 if ( !isset($_GET['subact']) )
 {
-	$news = $db->sql('SELECT * FROM `lib_news` ORDER BY `date` DESC LIMIT 0, 20;');
+	$news = $db->sql('SELECT * FROM `lib_news` ORDER BY `date` DESC LIMIT 0,5;');
 	while ( $n = mysql_fetch_assoc($news) )
 	{
 		echo '<div class=news><div class=p1>'.$n['title'].'</div><div class=p2>Автор: '.$n['autor'].' &nbsp;&nbsp;&nbsp; Дата: '.date('d.m.Y H:i', $n['date']).'.</div><div class=p3>'.nl2br($n['text']).'</div><!--div class=p3><a href="?act=1&subact='.$n['id'].'">Коментировать ('.$n['coment'].')</a></!--div--></div>';
@@ -299,13 +293,8 @@ elseif ( ($news = $db->sqla('SELECT * FROM `lib_news` WHERE `id`="'.$_GET['subac
 		$i++;
 		echo '<div class=news><div class=p3>'.nl2br($com['text']).'</div><div class=p2>Автор: '.$com['user'].' &nbsp;&nbsp;&nbsp; Дата: '.date('d.m.Y H:i', $com['date']).'. '.(($pers != false and $pers['priveleged']==1) ? '<a href="?act=1&subact='.$news['id'].'&del='.$com['id'].'">Удалить</a>' : '').'</div></div>';
 	}
-	//if ($i==0) echo '<br /><div class="p1" align="center">Комментарий не найдено.</div>';
-	/*if ($pers != true) echo '<table width="100%" class="but"><form method="post">
-		<tr><td><textarea name="ntext" class="inv_button" cols="50" rows="6"></textarea></td></tr>
-		<tr><td><center><input type="submit" class="login" value="Комментировать"></center></td></tr>
-		</form></table>';
-		*/
-} else echo '<b>Новость не найдена.</b><br /><br />';
+
+} 
 echo '</div>';
 ?>
 				<B>Обновленная главная страница</B>, <SMALL>06.11.2013 22:38</SMALL><BR>
