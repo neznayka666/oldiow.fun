0feb244075e3184f48cc861a004093a1f2a37e6a
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-09-09 09:46:39 +0300

    Update podarok.php

--- inc/locations/podarok.php
+++ inc/locations/podarok.php
@@ -18,6 +18,14 @@ if ($_GET["year"]=='1')
 		$msg = item_name_ny(333450);
 		$prize2='333465';
 		$msg2 = item_name_ny(333465);
+		$prize3='333465';
+		$msg3 = item_name_ny(333465);
+		$prize4='333465';
+		$msg4 = item_name_ny(333465);
+		$prize5='333465';
+		$msg5 = item_name_ny(333465);
+		$prize6='333465';
+		$msg6 = item_name_ny(333465);
 	}
 	/*
 	else if ($podarok=='2')
@@ -51,12 +59,13 @@ if ($_GET["year"]=='1')
 		$msg = '2000 IM';
 	}
 	*/
+	
 	if ($player->pers['podarok']=='0')
 	{
-		$db->sql("UPDATE `users` SET `podarok`='1', money=money+5000 WHERE uid='".$player->pers["uid"]."'");
+		$db->sql("UPDATE `users` SET `podarok`='1', money=money+50000 WHERE uid='".$player->pers["uid"]."'");
 		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize,$player->pers["uid"],-1,0,$player->pers["user"]));
 		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize2,$player->pers["uid"],-1,0,$player->pers["user"]));
-		echo"<font color=green><b>Администрация <b>oldiow.fun</b> поздравляет Вас с регистрацией и вручает подарок:) <br>Вы получили в подарок: ".$msg.",".$msg2."</b></font>";
+		echo"<font color=green><b>Администрация <b>oldiow.ru</b> поздравляет Вас с регистрацией и вручает подарок:) <br>Вы получили в подарок: ".$msg.",".$msg2."</b></font>";
 		say_to_chat ("s","Вы получили в подарок: <b>".$msg."</b>, <b>".$msg2."</b> и <b>5000</b> зм.",1,$player->pers["user"],'*',0);
 	}
 	else
@@ -78,6 +87,41 @@ if ($player->pers['podarok']=='0')
 	else {
 		echo"<font color=red><b>Вы уже получили подарок</b></font>";
 	}
+
+
+	if ($player->pers['podarok']=='1')
+	{
+		$db->sql("UPDATE `users` SET `podarok`='2', money=money+50000 WHERE uid='".$player->pers["uid"]."'");
+		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize,$player->pers["uid"],-1,0,$player->pers["user"]));
+		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize2,$player->pers["uid"],-1,0,$player->pers["user"]));
+		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize3,$player->pers["uid"],-1,0,$player->pers["user"]));
+		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize4,$player->pers["uid"],-1,0,$player->pers["user"]));
+		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize5,$player->pers["uid"],-1,0,$player->pers["user"]));
+		$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize6,$player->pers["uid"],-1,0,$player->pers["user"]));
+		echo"<font color=green><b>Администрация <b>oldiow.ru</b> поздравляет Вас с регистрацией и вручает подарок:) <br>Вы получили в подарок: ".$msg.",".$msg2."</b></font>";
+		say_to_chat ("s","Вы получили в подарок: <b>".$msg."</b>, <b>".$msg2."</b> и <b>50000</b> зм.",1,$player->pers["user"],'*',0);
+	}
+	else
+	{
+		echo"<font color=red><b>Вы уже получили подарок</b></font>";
+	}
+}
+
+if ($player->pers['podarok']=='1')
+	{
+?>
+            <p>Привет дорогой друг! <br>За твое терпение и преданость нашему миру, я для тебя приготовл награду.
+                <br>Спасибо за то, что ты с нами!
+            </p>
+            <input type=button onClick="location='main.php?year=1'" value="Получить Подарок!!!"
+                title="Чтоб получить подарок, нажмите на кнопку">
+            <br />
+            <?php
+	}
+	else {
+		echo"<font color=red><b>Вы уже получили подарок</b></font>";
+	}
+
 	?>
         </div>
         <div>
