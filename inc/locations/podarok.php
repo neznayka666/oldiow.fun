8e803fc6416b9a0749bcb062caff9d7e3f4c4129
Oleh <30344026+oleh-novakovskyi@users.noreply.github.com>
2021-09-10 18:57:31 +0300

    Update podarok.php

--- inc/locations/podarok.php
+++ inc/locations/podarok.php
@@ -1,9 +1,10 @@
 <div style="margin:25px auto;width:1200px;text-align:center;">
     <h3 class='titleCity'>Беседка</h3>
     <div style="position:relative;width:830px;height:329px;margin:0px auto;">
-        <img src="images/nav/bg/bes.png">
-        <div style="position:absolute;bottom:100px;left:0px;width:100%;font-size:20px;">
+			<img src="images/nav/bg/bes.png">
+			<div style="position:absolute;bottom:100px;left:0px;width:100%;font-size:20px;">
 <?php
+//подарок новичка
 function item_name_ny($id)
 {
 	$itm= $GLOBALS['db']->sqla("SELECT name FROM weapons WHERE id='".$id."'");
@@ -47,6 +48,55 @@ else {
 		echo"<font color=red><b>Вы уже получили подарок новичка</b></font>";
 	}
 	?>
-</div>
-</div>
+
+<?php
+/// годовой подарок
+function item_name_year($id)
+{
+	$itm= $GLOBALS['db']->sqla("SELECT name FROM weapons WHERE id='".$id."'");
+	return $itm['name'];
+}
+if ($_GET["year_gift"]=='1')
+{
+	$year_gift = rand(1,1);
+		if ($year_gift=='1')
+		{
+			$prize='333450';
+			$msg = item_name_year(333450);
+			$prize2='333465';
+			$msg2 = item_name_year(333465);
+			$money = 5000;
+		}
+	
+		if ($player->pers['year_gift']=='0')
+		{
+			$db->sql("UPDATE `users` SET `year_gift`='1', money=money+".$money." WHERE uid='".$player->pers["uid"]."'");
+			$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize,$player->pers["uid"],-1,0,$player->pers["user"]));
+			$db->sql("UPDATE `wp` SET `timeout`='' WHERE id=".insert_wp($prize2,$player->pers["uid"],-1,0,$player->pers["user"]));
+			echo"<font color=green><b>Администрация <b>oldiow.ru</b> поздравляет Вас с Годовщиной проекта и вручает подарок:) <br>Вы получили в подарок: ".$msg.",".$msg2."</b></font>";
+			say_to_chat ("s","Вы получили в подарок: <b>".$msg."</b>, <b>".$msg2."</b> и <b>".$money."</b> зм.",1,$player->pers["user"],'*',0);
+		}
+		else
+		{
+			echo"<font color=red><b>Вы уже получили подарок</b></font>";
+		}
+}
+if ($player->pers['year_gift']=='0')
+	{
+?>
+   <p>Привет дорогой друг! <br>За твое терпение и преданость нашему миру, я для тебя приготовл награду.
+   <br>Спасибо за то, что ты с нами!
+   </p>
+   <input type="button" onClick="location='main.php?year_gift=1'" value="Получить Подарок!!!" title="Чтоб получить подарок, нажмите на кнопку" class="inv_but">
+   <br />
+<?php
+	}
+else {
+		echo"<font color=red><b>Вы уже получили подарок</b></font>";
+	}
+	?>
+
+
+			</div>
+	</div>
 </div>
